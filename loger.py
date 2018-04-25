0aa1cd17cd429f55b02f77080aaa7d4ded27fb2d
Yacine <38400007+Redrrx@users.noreply.github.com>
2018-04-25 09:04:07 +0100

    Updated loger

--- loger.py
+++ loger.py
@@ -6,6 +6,8 @@ i = datetime.now()
 IP = socket.gethostbyname(socket.gethostname())
 vivox_ms=socket.gethostbyname('mpx5j.vivox.com')
 r6s_vcs=socket.gethostbyname('disp-rbswp-5-1.vivox.com')
+fn_vcs=socket.gethostbyname('disp-fnwp-5-1.vivox.com')
+
 # This is just some dumb logging
 # will improve in the future for more hardware/permission check incase Routing is not done proprely
 
@@ -13,7 +15,7 @@ def net_stuff():
 
     level = logging.INFO
     format = '  %(message)s'
-    handlers = [logging.FileHandler('log.log'), logging.StreamHandler()]
+    handlers = [logging.FileHandler('log.txt'), logging.StreamHandler()]
     logging.basicConfig(level=level, format=format, handlers=handlers)
     logging.info("----------------------------------------------------------------------------")
     logging.info(i.strftime('%Y/%m/%d %H:%M'))
@@ -21,7 +23,9 @@ def net_stuff():
     logging.info(vivox_ms)
     logging.info("VVX R6S MASTER SERVER IP IS:")
     logging.info(r6s_vcs)
-    public_ip = get('https://api.ipify.org').text
+    logging.info("VVX FN MASTER SERVER IP IS:")
+    logging.info(fn_vcs)
+    public_ip = get('https://api.ipify.org').text #why tf is this variable here ?
     logging.info("IP AFTER VPN CONNECTION IS:")
     logging.info(public_ip)
     logging.info("----------------------------------------------------------------------------")
