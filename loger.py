3e8a9e4c0175bd5a8a63d79ffd4362bdc39b5e47
Yacine <38400007+Redrrx@users.noreply.github.com>
2018-04-30 20:24:00 +0100

    Updated loger.py to v2

--- loger.py
+++ loger.py
@@ -1,15 +1,27 @@
 import logging
 from datetime import datetime
+import requests
+import time
 from requests import get
 import socket
+from bs4 import BeautifulSoup
 i = datetime.now()
 IP = socket.gethostbyname(socket.gethostname())
 vivox_ms=socket.gethostbyname('mpx5j.vivox.com')
 r6s_vcs=socket.gethostbyname('disp-rbswp-5-1.vivox.com')
 fn_vcs=socket.gethostbyname('disp-fnwp-5-1.vivox.com')
 
+
+
 # This is just some dumb logging
 # will improve in the future for more hardware/permission check incase Routing is not done proprely
+
+#vpn disconnects due inactivity this is a temporary workaround will change in future version
+def signal():
+    response = requests.get('https://api.ipify.org')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print('Sending signal to vpn server every 60 seconds, current ip is: ' + soup.prettify() + "Response code: "+ str(response.status_code) + " " + time.ctime())
+    time.sleep(60)
 
 def net_stuff():
 
@@ -25,10 +37,10 @@ def net_stuff():
     logging.info(r6s_vcs)
     logging.info("VVX FN MASTER SERVER IP IS:")
     logging.info(fn_vcs)
-    public_ip = get('https://api.ipify.org').text #why tf is this variable here ?
-    logging.info("IP AFTER VPN CONNECTION IS:")
-    logging.info(public_ip)
-    logging.info("----------------------------------------------------------------------------")
+
+
 def run_log():
     net_stuff()
-    logging.shutdown()
+    time.sleep(3)
+    while True:
+        signal()
